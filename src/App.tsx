b156ed684b1f47a95a836dc46d2f6f80dc7c2095
nikhil-mahto <nikhil@sparkbrains.in>
2025-04-09 10:03:59 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import LoginScreen from "./pages/authentication/Login";
+
+function App() {
+  return <LoginScreen />;
+}
+
+export default App;
---
+++ src/pages/authentication/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/login.css"; 
+
+const LoginScreen: React.FC = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="login-title">Login</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
